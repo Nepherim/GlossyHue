6d8825c3caf140b6bb3613f99bd1141d2620d3f1
David Gilbert <david@solidgone.com>
2009-08-11 02:47:23 +0000

    chg: bi_null is now a condition not a markup expression. new: Switched to use bi_auth. chg: Updated to cross-skin skin.php format.

--- skin.php
+++ skin.php
@@ -1,54 +1,47 @@
 <?php if (!defined('PmWiki')) exit();
 /* PmWiki GlossyHue skin
  *
- * Examples at: http://pmwiki.com/Cookbook/GlossyHue and http://solidgone.com/Skins/
- * Based on Glossy Blue by Nick La http://www.ndesign-studio.com/
+ * Examples at: http://pmwiki.com/Cookbook/GlossyHue and http://solidgone.org/Skins/
  * Copyright (c) 2009 David Gilbert
- * Dual licensed under the MIT and GPL licenses:
- *    http://www.opensource.org/licenses/mit-license.php
- *    http://www.gnu.org/licenses/gpl.html
+ * This work is licensed under a Creative Commons Attribution-Share Alike 3.0 United States License.
+ * Please retain the links in the footer.
+ * http://creativecommons.org/licenses/by-sa/3.0/us/
  */
 global $FmtPV;
 $FmtPV['$SkinName'] = '"GlossyHue"';
 $FmtPV['$SkinVersion'] = '"2.0.0"';
 
-# Create a nosearch markup, since one doesn't exist
-Markup('nosearch', 'directives',  '/\\(:nosearch:\\)/ei', "SetTmplDisplay('PageSearchFmt',0)");
-$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
-Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
-
-# ----------------------------------------
-# - Standard Skin Setup
-# ----------------------------------------
 global $PageLogoUrl, $PageLogoUrlHeight, $PageLogoUrlWidth, $HTMLStylesFmt;
 if (!empty($PageLogoUrl)) {
-	if (!isset($PageLogoUrlWidth) || !isset($PageLogoUrlHeight)) {
-		$size = getimagesize($PageLogoUrl);
-		SDV($PageLogoUrlWidth, ($size ?$size[0]+15 :0) .'px');
-		SDV($PageLogoUrlHeight, ($size ?$size[1] :0) .'px');
-	}
+	dg_SetLogoHeightWidth(15);
 	$HTMLStylesFmt['glossyhue'] .=
 		'#siteheader .sitetitle a{height:' .$PageLogoUrlHeight .'; background: url(' .$PageLogoUrl .') left top no-repeat} '.
 		'#siteheader .sitetitle a, #siteheader .sitetag{padding-left: ' .$PageLogoUrlWidth .'} '.
 		'#siteheader .sitetag{margin-top: ' .(35-substr($PageLogoUrlHeight,0,-2)) .'px}';
 }
+$SkinColor = dg_SetSkinColor('blue', array('blue'));
 
+# ----------------------------------------
+# - Standard Skin Setup
+# ----------------------------------------
 $FmtPV['$WikiTitle'] = '$GLOBALS["WikiTitle"]';
 $FmtPV['$WikiTag'] = '$GLOBALS["WikiTag"]';
+
+# Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
+$FmtPV['$RightColumn'] = "\$GLOBALS['TmplDisplay']['PageRightFmt']";
+Markup('noright', 'directives',  '/\\(:noright:\\)/ei', "SetTmplDisplay('PageRightFmt',0)");
+$FmtPV['$ActionBar'] = "\$GLOBALS['TmplDisplay']['PageActionFmt']";
+Markup('noaction', 'directives',  '/\\(:noaction:\\)/ei', "SetTmplDisplay('PageActionFmt',0)");
+$FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
+Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
+$FmtPV['$SearchBar'] = "\$GLOBALS['TmplDisplay']['PageSearchFmt']";
+Markup('nosearch', 'directives',  '/\\(:nosearch:\\)/ei', "SetTmplDisplay('PageSearchFmt',0)");
+Markup('fieldset', 'inline', '/\\(:fieldset:\\)/i', "<fieldset>");
+Markup('fieldsetend', 'inline', '/\\(:fieldsetend:\\)/i', "</fieldset>");
 
 # Define a link stye for new page links
 global $LinkPageCreateFmt;
 SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
-
-# Default color scheme
-global $SkinColor, $ValidSkinColors;
-if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
-array_push($ValidSkinColors, 'blue');
-if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) ) {
-	$SkinColor = $_GET['color'];
-} elseif ( !in_array($SkinColor, $ValidSkinColors) ) {
-	$SkinColor = 'blue';
-}
 
 # Override pmwiki styles otherwise they will override styles declared in css
 global $HTMLStylesFmt;
@@ -60,3 +53,27 @@ $PageStorePath = dirname(__FILE__)."/wikilib.d/{\$FullName}";
 $where = count($WikiLibDirs);
 if ($where>1) $where--;
 array_splice($WikiLibDirs, $where, 0, array(new PageStore($PageStorePath)));
+
+# ----------------------------------------
+# - Standard Skin Functions
+# ----------------------------------------
+function dg_SetSkinColor($default, $valid_colors){
+global $SkinColor, $ValidSkinColors, $_GET;
+	if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
+	$ValidSkinColors = array_merge($ValidSkinColors, $valid_colors);
+	if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) )
+		$SkinColor = $_GET['color'];
+	elseif ( !in_array($SkinColor, $ValidSkinColors) )
+		$SkinColor = $default;
+	return $SkinColor;
+}
+
+# Determine logo height and width
+function dg_SetLogoHeightWidth ($wPad, $hPad=0){
+global $PageLogoUrl, $PageLogoUrlHeight, $PageLogoUrlWidth;
+	if (!isset($PageLogoUrlWidth) || !isset($PageLogoUrlHeight)){
+		$size = @getimagesize($PageLogoUrl);
+		if (!isset($PageLogoUrlWidth))  SDV($PageLogoUrlWidth, ($size ?$size[0]+$wPad :0) .'px');
+		if (!isset($PageLogoUrlHeight))  SDV($PageLogoUrlHeight, ($size ?$size[1]+$hPad :0) .'px');
+	}
+}

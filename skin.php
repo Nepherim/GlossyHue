2f95077b954f6cd893da0e2b46d78a9c7b59f838
David Gilbert <david@solidgone.com>
2009-09-29 04:38:09 +0000

    * new: Added internationalization support via Site.XLPage-marinee. * bug: Search uses POST rather than GET which causes searches to return to home page. (reported by Orin) * chg: Improved BlogIt styles. * chg: Minor styling improvements (line-height, wikitext margins). * bug: Page header cannot be removed with noheader markup.

--- skin.php
+++ skin.php
@@ -9,7 +9,7 @@
  */
 global $FmtPV;
 $FmtPV['$SkinName'] = '"GlossyHue"';
-$FmtPV['$SkinVersion'] = '"2.0.0"';
+$FmtPV['$SkinVersion'] = '"2.1.0"';
 
 global $PageLogoUrl, $PageLogoUrlHeight, $PageLogoUrlWidth, $HTMLStylesFmt;
 if (!empty($PageLogoUrl)) {
